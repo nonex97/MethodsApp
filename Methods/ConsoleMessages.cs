20cee2154969179c6fb3ba56cf69b43ddd5f4439
nonex97 <ngrguric97@gmail.com>
2026-06-11 22:26:31 +0200

    Learned about methods, created a sample method and called it from the Program.cs

---
+++ Methods/ConsoleMessages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Methods
+{
+	public static class ConsoleMessages // can be public (anyone can use), internal (anyone inside this project), private (anyone inside of this scope)
+	{
+		public static void SayHi()
+		{
+            Console.WriteLine("Hello User!");
+            Console.WriteLine("I hope you're having a good day!");
+		}
+	}
+}
+
+// or (ONLY .NET 6.0 and after), there can be only one namespace anyway and you lose one set of curly braces
+
+//namespace Methods
+//public static class SampleMethods
+//{
+
+//}
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,24 @@
-﻿
+﻿using Methods; // importing the Methods namespace
 
+//Console.WriteLine(); // this is a method
 
+//for (int i = 0; i < 10; i++)
+//{
+//    SampleMethods.SayHi();
+//}
 
+ConsoleMessages.SayHi();
+
+Console.WriteLine("This is Neven.");
+
+ConsoleMessages.SayHi();
+
+// or (could appear in code but not recommended to use)
+
+//using static Methods.SampleMethods;
+//SayHi();
+
+// could also be used for Console.WriteLine();
+
+//using static System.Console;
+//WriteLine();
